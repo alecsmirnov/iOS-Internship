9a6de131ee7d10dbecc6bedd91e19ccc50b277ce
alecsmirnov <alecsmirnov93@gmail.com>
2020-07-03 13:55:40 +0700

    Unwrap data in Statistics

--- TableManagement/TableManagement/StatisticsViewController.swift
+++ TableManagement/TableManagement/StatisticsViewController.swift
@@ -22,8 +22,8 @@ class StatisticsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         countLabel.text = String("Items count: \(tableData.count)")
-        maxValueLabel.text = String("Max value: \(tableData.max())")
-        minValueLabel.text = String("Min value: \(tableData.min())")
+        maxValueLabel.text = String("Max value: \(tableData.max()!)")
+        minValueLabel.text = String("Min value: \(tableData.min()!)")
         averageLabel.text = String(format: "Average: %.3f ", Double(tableData.reduce(0, +)) / Double(tableData.count))
     }
 }
