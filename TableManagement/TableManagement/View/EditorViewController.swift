b8fe0abb2d97d93402f1bb3f26e31b11a7089dfc
alecsmirnov <alecsmirnov93@gmail.com>
2020-07-24 16:30:49 +0700

    EditorViewController cleaning

--- TableManagement/TableManagement/View/EditorViewController.swift
+++ TableManagement/TableManagement/View/EditorViewController.swift
@@ -122,52 +122,12 @@ class EditorViewController: UIViewController {
 
 extension EditorViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        var isValid = false
+        var validInput = false
         
-        if string.isEmpty {
-            isValid = true
+        if let text = textField.text {
+            validInput = editorViewModel.isValidUserInput(text: text, string: string)
         }
         
-        guard let text = textField.text else {
-            return false
-        }
-        
-        if !isValid {
-            let numberMax = 100
-            let filtered = string.filter("0123456789".contains)
-            
-            var integer = abs((text as NSString).integerValue)
-            if let firstIndex = text.firstIndex(of: ".") {
-                integer = (text[..<firstIndex] as NSString).integerValue
-            }
-            else {
-                if string != "." {
-                    integer = integer * 10 + (string as NSString).integerValue
-                }
-            }
-            
-            switch string {
-            case filtered:
-                isValid = true
-            case ".":
-                let dotsCount = text.components(separatedBy: ".").count
-                if dotsCount <= 1 && integer < numberMax {
-                    isValid = true
-                }
-            case "-":
-                let signsCount = text.components(separatedBy: "-").count
-                if text.isEmpty && signsCount <= 1 {
-                    isValid = true
-                }
-            default:
-                isValid = false
-            }
-            
-            if numberMax < integer {
-                isValid = false
-            }
-        }
-        
-        return isValid
+        return validInput
     }
 }
--- TableManagement/TableManagement/ViewModel/EditorViewModel.swift
+++ TableManagement/TableManagement/ViewModel/EditorViewModel.swift
@@ -59,17 +59,63 @@ class EditorViewModel {
         textColor.blue = value / 255.0
     }
     
-    func userAddedNewNumber(_ text: String) {
-        delegate.editorViewModelDelegateAddNumber(self, number: Number(value: (text as NSString).floatValue, color: textColor))
+    func userAddedNewNumber(_ textNumber: String) {
+        delegate.editorViewModelDelegateAddNumber(self, number: Number(value: (textNumber as NSString).floatValue, color: textColor))
         
         textColor = Color(red: 0, green: 0, blue: 0)
     }
     
-    func userChangedSelectedNumber(_ text: String) {
-        delegate.editorViewModelDelegateChangeSelectedNumber(self, newNumber: Number(value: (text as NSString).floatValue, color: textColor))
+    func userChangedSelectedNumber(_ textNumber: String) {
+        delegate.editorViewModelDelegateChangeSelectedNumber(self, newNumber: Number(value: (textNumber as NSString).floatValue, color: textColor))
     }
     
     func userDeletedSelectedNumber() {
         delegate.editorViewModelDelegateDeleteSelectedNumber(self)
+    }
+    
+    func isValidUserInput(text: String, string: String) -> Bool {
+        var valid = false
+        
+        if string.isEmpty {
+            valid = true
+        }
+        
+        if !valid {
+            let numberMax = 100
+            let filtered = string.filter("0123456789".contains)
+            
+            var integer = abs((text as NSString).integerValue)
+            if let firstIndex = text.firstIndex(of: ".") {
+                integer = (text[..<firstIndex] as NSString).integerValue
+            }
+            else {
+                if string != "." {
+                    integer = integer * 10 + (string as NSString).integerValue
+                }
+            }
+            
+            switch string {
+            case filtered:
+                valid = true
+            case ".":
+                let dotsCount = text.components(separatedBy: ".").count
+                if dotsCount <= 1 && integer < numberMax {
+                    valid = true
+                }
+            case "-":
+                let signsCount = text.components(separatedBy: "-").count
+                if text.isEmpty && signsCount <= 1 {
+                    valid = true
+                }
+            default:
+                valid = false
+            }
+            
+            if numberMax < integer {
+                valid = false
+            }
+        }
+        
+        return valid
     }
 }
